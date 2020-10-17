0354ef3e4cfc1980ac31d587e4e028bca5bd973b
Deep-The-Great <71218265+Deep-The-Great@users.noreply.github.com>
2020-10-18 00:42:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+import discord
+from discord.ext import commands
+import os
+import datetime
+import asyncio
+import random
+
+
+client = commands.Bot(command_prefix=">")
+
+@client.event
+async def on_ready():
+	print("Bot is ready")
+
+@client.command()
+async def hello(ctx):
+	await ctx.send("Hi")
+
+@client.command()
+@commands.has_role("Snipers")
+async def gstart(ctx, mins : int, * , prize: str):
+    embed = discord.Embed(title = "Giveaway!", description = f"{prize}", color = ctx.author.color)
+
+    end = datetime.datetime.utcnow() + datetime.timedelta(seconds = mins*60) 
+
+    embed.add_field(name = "Ends At:", value = f"{end} UTC")
+    embed.set_footer(text = f"Ends {mins} mintues from now!")
+
+    my_msg = await ctx.send(embed = embed)
+
+
+    await my_msg.add_reaction("🎉")
+
+
+    await asyncio.sleep(mins*60)
+
+
+    new_msg = await ctx.channel.fetch_message(my_msg.id)
+
+
+    users = await new_msg.reactions[0].users().flatten()
+    users.pop(users.index(client.user))
+
+    winner = random.choice(users)
+
+    await ctx.send(f"Congratulations! {winner.mention} won {prize}!")
+
+  
+
+client.run(os.getenv('TOKEN'))
+
