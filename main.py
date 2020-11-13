21147a275c595682f7744b1a7e033fa53b98a8c4
Deep-The-Great <71218265+Deep-The-Great@users.noreply.github.com>
2020-11-13 21:51:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,109 +46,13 @@ async def help(ctx):
     embed.add_field(name='**MODERATION CATEGORY**', value='```purge (amount) | kick (user) | ban (user) | unban (user) | nuke | addrole (user) (role name) | removerole (user) (role name) |```', inline=False)
     embed.add_field(name='**TICKET CATEGORY**', value='```setupticket | openticket | closeticket | (openticket and closeticket commands will work when ticket setup has completed)```', inline=False)
     embed.add_field(name='**FUN**', value='```howgay (user) | cat | meme |```', inline=False)
-    embed.add_field(name='**OTHER**', value='```say (message) | invite | giveaway |```')
+    embed.add_field(name='**OTHER**', value='```say (message) | invite |```')
     #embed.add_field(name='**ADMIN**', value='```invite |```')
     embed.set_footer(text='Made by Team Hino')
 
     await ctx.send(embed=embed)
 
-#giveaway 
 
-def convert(time):
-    pos = ["s","m","h","d"]
-
-    time_dict = {"s" : 1, "m" : 60, "h" : 3600 , "d" : 3600*24}
-
-    unit = time[-1]
-
-    if unit not in pos:
-        return -1
-    try:
-        val = int(time[:-1])
-    except:
-        return -2
-
-
-    return val * time_dict[unit]
-
-@client.command()
-@commands.has_role("Giveaways")
-async def giveaway(ctx):
-    await ctx.send("Let's start with this giveaway! Answer these questions within 15 seconds!")
-
-    questions = ["Which channel should it be hosted in?", 
-                "What should be the duration of the giveaway? (s|m|h|d)",
-                "What is the prize of the giveaway?"]
-
-    answers = []
-
-    def check(m):
-        return m.author == ctx.author and m.channel == ctx.channel 
-
-    for i in questions:
-        await ctx.send(i)
-
-        try:
-            msg = await client.wait_for('message', timeout=15.0, check=check)
-        except asyncio.TimeoutError:
-            await ctx.send('You didn\'t answer in time, please be quicker next time!')
-            return
-        else:
-            answers.append(msg.content)
-    try:
-        c_id = int(answers[0][2:-1])
-    except:
-        await ctx.send(f"You didn't mention a channel properly. Do it like this {ctx.channel.mention} next time.")
-        return
-
-    channel = client.get_channel(c_id)
-
-    time = convert(answers[1])
-    if time == -1:
-        await ctx.send(f"You didn't answer the time with a proper unit. Use (s|m|h|d) next time!")
-        return
-    elif time == -2:
-        await ctx.send(f"The time must be an integer. Please enter an integer next time")
-        return            
-
-    prize = answers[2]
-
-    await ctx.send(f"The Giveaway will be in {channel.mention} and will last {answers[1]}!")
-
-
-    embed = discord.Embed(title = "Giveaway!", description = f"{prize}", color = ctx.author.color)
-
-    embed.add_field(name = "Hosted by:", value = ctx.author.mention)
-
-    embed.set_footer(text = f"Ends {answers[1]} from now!")
-
-    my_msg = await channel.send(embed = embed)
-
-
-    await my_msg.add_reaction("🎉")
-
-
-    await asyncio.sleep(time)
-
-
-    new_msg = await channel.fetch_message(my_msg.id)
-
-
-    users = await new_msg.reactions[0].users().flatten()
-    users.pop(users.index(client.user))
-
-    winner = random.choice(users)
-
-    await channel.send(f"Congratulations! {winner.mention} won {prize}!")
-
-@giveaway.error
-async def giveaway_error(ctx, error):
-    if isinstance(error, commands.CheckFailure):
-        embed = discord.Embed(colour= discord.Colour.red())
-        embed.add_field(name=':x: **Giveaway Error**\n', value='You need role called Giveaways to perform this action', inline=False)
-        embed.set_footer(text='Made by Team Hino')
-
-        await ctx.send(embed = embed)
         
 # Setup ticket
 
