2e35fa3b31e0b5bbe55f160eb693d0fdb5e935d0
Deep-The-Great <71218265+Deep-The-Great@users.noreply.github.com>
2020-11-14 18:32:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,10 @@ client = commands.Bot(command_prefix="h!")
 
 @client.event
 async def on_ready():
-    print('Bot is ready.')
+    await client.change_presence(status=discord.Status.online, activity=discord.Activity(
+        type=discord.ActivityType.watching, name=f"on {len(client.guilds)} servers | h!help"
+    ))
+    print("Ready")
 
 
 #invite
